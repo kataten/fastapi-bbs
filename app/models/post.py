d482145f6126dfc9213917cd6661aa0bc703a1bc
片山天雅 <ckg24090@st.ckg.ac.jp>
2025-12-02 11:13:29 +0900

    Fix:不具合修正(routers/threads.py)
    routers/threads.pyのGET /{thread_id}が存在しない場合の処理を追加
    schemas/post.pyのcreated_atをstrからdatetimeへ変更
    database.py DB_PATHの固定化

--- app/models/post.py
+++ app/models/post.py
@@ -5,7 +5,7 @@ from app.database import Base
 class Post(Base):
     __tablename__ = "posts"
     
-    id = Column(Integer, prymary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     thread_id = Column(Integer, ForeignKey("threads.id"), nullable=False)
     post_number = Column(Integer, nullable=False)
     parent_post_id = Column(Integer, ForeignKey("posts.id"), nullable=True)
